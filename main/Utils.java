42c6a16f7ab67803131320a28e9936566311eabe
thakurpriyesh <155248721+thakurpriyesh@users.noreply.github.com>
2025-11-01 15:58:27 +0530

    Delete main directory

--- main/Utils.java
+++
@@ -1,8 +0,0 @@
-// Utils.java
-// Utility functions for the project.
-
-public class Utils {
-    public static void greetUser(String name) {
-        System.out.println("Hello, " + name + "! Let's track some progress!");
-    }
-}
